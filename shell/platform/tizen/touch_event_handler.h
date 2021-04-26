d9aad2ab26eeba7162d08d36526fe1213d37ca7e
Rafal Walczyna <r.walczyna@samsung.com>
2021-04-26 11:57:32 +0200

    Add multi touch support for Tizen embedder
    Tested on TM1 device
    
    Signed-off-by: Rafal Walczyna <r.walczyna@samsung.com>

--- shell/platform/tizen/touch_event_handler.h
+++ shell/platform/tizen/touch_event_handler.h
@@ -27,7 +27,7 @@ class TouchEventHandler {
 
   void SendFlutterPointerEvent(FlutterPointerPhase phase, double x, double y,
                                double scroll_delta_x, double scroll_delta_y,
-                               size_t timestamp);
+                               size_t timestamp, int multi_touch_device);
 
   static Eina_Bool OnTouch(void* data, int type, void* event);
 };
