4803e5a2dffa7a891c005c5c9394e371b19ec806
Rafal Walczyna <r.walczyna@samsung.com>
2021-07-01 14:43:31 +0200

    [AppControl] Add basic operations for AppControl
    Signed-off-by: Rafal Walczyna <r.walczyna@samsung.com>

--- shell/platform/tizen/channels/app_control_channel.cc
+++ shell/platform/tizen/channels/app_control_channel.cc
@@ -12,7 +12,7 @@ static constexpr char kEventChannelName[] = "tizen/app_control_event";
 int AppControl::next_id_ = 0;
 
 AppControlChannel::AppControlChannel(BinaryMessenger* messenger) {
-  FT_LOGD("AppControlChannel");
+  FT_LOGE("AppControlChannel");
   method_channel_ = std::make_unique<MethodChannel<EncodableValue>>(
       messenger, kChannelName, &StandardMethodCodec::GetInstance());
 
@@ -28,13 +28,13 @@ AppControlChannel::AppControlChannel(BinaryMessenger* messenger) {
           [this](const flutter::EncodableValue* arguments,
                  std::unique_ptr<flutter::EventSink<>>&& events)
               -> std::unique_ptr<flutter::StreamHandlerError<>> {
-            FT_LOGD("OnListen");
+            FT_LOGE("OnListen");
             RegisterEventHandler(std::move(events));
             return nullptr;
           },
           [this](const flutter::EncodableValue* arguments)
               -> std::unique_ptr<flutter::StreamHandlerError<>> {
-            FT_LOGD("OnCancel");
+            FT_LOGE("OnCancel");
             UnregisterEventHandler();
             return nullptr;
           });
@@ -45,20 +45,21 @@ AppControlChannel::AppControlChannel(BinaryMessenger* messenger) {
 AppControlChannel::~AppControlChannel() {}
 
 void AppControlChannel::NotifyAppControl(app_control_h app_control) {
-  FT_LOGD("NotifyAppControl");
+  FT_LOGE("NotifyAppControl");
   auto app = std::make_unique<AppControl>(app_control);
   if (!events_) {
-    queue_.push(std::move(app));
+    queue_.push(app->GetId());
     FT_LOGE("EventChannel not set yet");
   } else {
-    events_->Success(EncodableValue(app->GetOperation()));
+    events_->Success(EncodableValue(app->GetId()));
   }
+  map_.insert({app->GetId(), std::move(app)});
 }
 
 void AppControlChannel::HandleMethodCall(
     const MethodCall<EncodableValue>& method_call,
     std::unique_ptr<MethodResult<EncodableValue>> result) {
-  FT_LOGD("HandleMethodCall : %s", method_call.method_name().data());
+  FT_LOGE("HandleMethodCall : %s", method_call.method_name().data());
   // const auto& arguments = *method_call.arguments();
 
   if (method_call.method_name().compare("GetOperation") == 0) {
@@ -70,17 +71,20 @@ void AppControlChannel::HandleMethodCall(
 
 void AppControlChannel::RegisterEventHandler(
     std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> events) {
+  FT_LOGE("RegisterEventHandler");
   events_ = std::move(events);
   SendAlreadyQueuedEvents();
 }
 
 void AppControlChannel::UnregisterEventHandler() {
+  FT_LOGE("UnregisterEventHandler");
   events_.reset();
 }
 
 void AppControlChannel::SendAlreadyQueuedEvents() {
+  FT_LOGE("HandleMethodCall: %d", queue_.size());
   while (!queue_.empty()) {
-    events_->Success(EncodableValue(queue_.front()->GetOperation()));
+    events_->Success(EncodableValue(queue_.front()));
     queue_.pop();
   }
 }
@@ -91,13 +95,167 @@ void AppControlChannel::GetOperation(
     result->Error("No app_control");
     return;
   }
-  std::string operation = queue_.front()->GetOperation();
-  if (operation.empty()) {
-    result->Error("Could not get operation");
+  result->Success(EncodableValue(queue_.size()));
+  return;
+}
+
+AppControl::AppControl(app_control_h app_control) : id_(next_id_++) {
+  FT_LOGE("AppControl construct: %d", id_);
+  int ret = app_control_clone(&handle_, app_control);
+  if (ret != APP_CONTROL_ERROR_NONE) {
+    FT_LOGE("Could not clone app control handle");
+    handle_ = nullptr;
     return;
   }
-  result->Success(EncodableValue(operation));
-  return;
+}
+
+AppControl::~AppControl() {
+  FT_LOGE("AppControl destruct: %d", id_);
+  app_control_destroy(handle_);
+}
+
+AppControlResult AppControl::GetString(std::string& str,
+                                       int func(app_control_h, char**)) {
+  FT_LOGD("AppControl::GetString");
+  char* op;
+  int ret = func(handle_, &op);
+  if (ret != APP_CONTROL_ERROR_NONE) {
+    return AppControlResult(ret);
+  }
+  str = std::string{op};
+  free(op);
+  return AppControlResult(APP_CONTROL_ERROR_NONE);
+}
+
+AppControlResult AppControl::SetString(const std::string& str,
+                                       int func(app_control_h, const char*)) {
+  FT_LOGD("AppControl::SetString");
+  int ret = func(handle_, str.c_str());
+  return AppControlResult(ret);
+}
+
+bool _app_control_extra_data_cb(app_control_h app,
+                                const char* key,
+                                void* user_data) {
+  auto extra_data = static_cast<AppControlExtraData*>(user_data);
+  bool is_array = false;
+  int ret = app_control_is_extra_data_array(app, key, &is_array);
+  if (ret != APP_CONTROL_ERROR_NONE) {
+    FT_LOGE("app_control_is_extra_data_array() failed at key %s", key);
+    return false;
+  }
+
+  if (is_array) {
+    char** strings = NULL;
+    int length = 0;
+    ret = app_control_get_extra_data_array(app, key, &strings, &length);
+    if (ret != APP_CONTROL_ERROR_NONE) {
+      FT_LOGE("app_control_get_extra_data() failed at key %s", key);
+      return false;
+    }
+    std::vector<std::string> vec;
+    for (int i = 0; i < length; i++) {
+      vec.push_back(strings[i]);
+      free(strings[i]);
+    }
+    free(strings);
+    extra_data->Add(key, vec);
+  } else {
+    char* value;
+    ret = app_control_get_extra_data(app, key, &value);
+    if (ret != APP_CONTROL_ERROR_NONE) {
+      FT_LOGE("app_control_get_extra_data() failed at key %s", key);
+      return false;
+    }
+    extra_data->Add(key, value);
+    free(value);
+  }
+
+  return true;
+}
+
+AppControlResult AppControl::ReadExtraData() {
+  int ret = app_control_foreach_extra_data(handle_, _app_control_extra_data_cb,
+                                           &extra_data_);
+  return AppControlResult(ret);
+}
+
+AppControlResult AppControl::GetOperation(std::string& operation) {
+  return GetString(operation, app_control_get_operation);
+}
+
+AppControlResult AppControl::SetOperation(const std::string& operation) {
+  return SetString(operation, app_control_set_operation);
+}
+
+AppControlResult AppControl::GetUri(std::string& uri) {
+  return GetString(uri, app_control_get_uri);
+}
+
+AppControlResult AppControl::SetUri(const std::string& uri) {
+  return SetString(uri, app_control_set_uri);
+}
+
+AppControlResult AppControl::GetMime(std::string& mime) {
+  return GetString(mime, app_control_get_mime);
+}
+
+AppControlResult AppControl::SetMime(const std::string& mime) {
+  return SetString(mime, app_control_set_mime);
+}
+
+AppControlResult AppControl::GetCategory(std::string& category) {
+  return GetString(category, app_control_get_category);
+}
+
+AppControlResult AppControl::SetCategory(const std::string& category) {
+  return SetString(category, app_control_set_category);
+}
+
+AppControlResult AppControl::GetAppId(std::string& app_id) {
+  return GetString(app_id, app_control_get_app_id);
+}
+
+AppControlResult AppControl::SetAppId(const std::string& app_id) {
+  return SetString(app_id, app_control_set_app_id);
+}
+
+AppControlResult AppControl::GetComponentId(std::string& component_id) {
+  // Since 5.5
+  return GetString(component_id, app_control_get_component_id);
+}
+
+AppControlResult AppControl::SetComponentId(const std::string& component_id) {
+  // Since 5.5
+  return SetString(component_id, app_control_set_component_id);
+}
+
+AppControlResult AppControl::GetCaller(std::string& caller) {
+  return GetString(caller, app_control_get_caller);
+}
+
+AppControlResult AppControl::GetLaunchMode(LaunchMode& launch_mode) {
+  app_control_launch_mode_e launch_mode_e;
+  int ret = app_control_get_launch_mode(handle_, &launch_mode_e);
+  if (ret != APP_CONTROL_ERROR_NONE) {
+    return AppControlResult(ret);
+  }
+  launch_mode = static_cast<LaunchMode>(launch_mode_e);
+  return AppControlResult(APP_CONTROL_ERROR_NONE);
+}
+
+AppControlResult AppControl::SetLaunchMode(const LaunchMode launch_mode) {
+  app_control_launch_mode_e launch_mode_e =
+      static_cast<app_control_launch_mode_e>(launch_mode);
+  int ret = app_control_set_launch_mode(handle_, launch_mode_e);
+  return AppControlResult(ret);
+}
+
+AppControlResult AppControl::Reply(AppControl* reply, Result result) {
+  app_control_result_e result_e = static_cast<app_control_result_e>(result);
+  int ret = app_control_reply_to_launch_request(reply->Handle(), this->handle_,
+                                                result_e);
+  return AppControlResult(ret);
 }
 
 }  // namespace flutter
--- shell/platform/tizen/channels/app_control_channel.h
+++ shell/platform/tizen/channels/app_control_channel.h
@@ -7,6 +7,7 @@
 
 #include <app.h>
 #include <queue>
+#include <unordered_map>
 
 #include "flutter/shell/platform/common/client_wrapper/include/flutter/binary_messenger.h"
 #include "flutter/shell/platform/common/client_wrapper/include/flutter/encodable_value.h"
@@ -19,38 +20,113 @@ class FlutterTizenEngine;
 
 namespace flutter {
 
-class AppControl {
+using EncodableList = std::vector<EncodableValue>;
+using EncodableMap = std::map<EncodableValue, EncodableValue>;
+
+struct AppControlResult {
+  AppControlResult() : error_code(APP_CONTROL_ERROR_NONE){};
+  AppControlResult(int code) : error_code(code) {}
+
+  // Returns false on error
+  operator bool() const { return APP_CONTROL_ERROR_NONE == error_code; }
+
+  std::string message() { return get_error_message(error_code); }
+
+  int error_code;
+};
+
+class AppControlExtraData {
  public:
-  AppControl(app_control_h app_control) : id_(next_id_++) {
-    FT_LOGD("AppControl construct: %d", id_);
-    int ret = app_control_clone(&app_control_, app_control);
-    if (ret != APP_CONTROL_ERROR_NONE) {
-      FT_LOGE("Could not clone app control handle");
-      app_control_ = nullptr;
-      return;
+  AppControlExtraData() {}
+  ~AppControlExtraData() {}
+
+  void Add(std::string key, std::string value) {
+    if (strings_lists_.find(key) != strings_lists_.end()) {
+      strings_lists_.erase(key);
     }
+    strings_[key] = value;
   }
 
-  ~AppControl() {
-    FT_LOGD("AppControl destruct: %d", id_);
-    app_control_destroy(app_control_);
+  void Add(std::string key, std::vector<std::string> value) {
+    if (strings_.find(key) != strings_.end()) {
+      strings_.erase(key);
+    }
+    strings_lists_[key] = value;
   }
 
-  std::string GetOperation() {
-    FT_LOGD("AppControl::GetOperation");
-    char* op;
-    int ret = app_control_get_operation(app_control_, &op);
-    if (ret != APP_CONTROL_ERROR_NONE) {
-      FT_LOGE("Could not get operation");
-      return "";
-    }
-    std::string operation{op};
-    free(op);
-    return operation;
+  void Remove(std::string key) {
+    strings_.erase(key);
+    strings_lists_.erase(key);
   }
+
+  bool Has(std::string key) {
+    if (strings_.find(key) != strings_.end()) {
+      return true;
+    }
+    if (strings_lists_.find(key) != strings_lists_.end()) {
+      return true;
+    }
+    return false;
+  }
+
+  std::vector<std::string>& GetList(std::string key) {
+    return strings_lists_[key];
+  }
+
+  std::string& GetString(std::string key) { return strings_[key]; }
+
+  size_t Size() { return strings_.size() + strings_lists_.size(); }
 
  private:
-  app_control_h app_control_;
+  std::unordered_map<std::string, std::string> strings_;
+  std::unordered_map<std::string, std::vector<std::string>> strings_lists_;
+};
+
+class AppControl {
+ public:
+  enum LaunchMode {
+    Single = APP_CONTROL_LAUNCH_MODE_SINGLE,
+    Group = APP_CONTROL_LAUNCH_MODE_GROUP
+  };
+  enum Result {
+    Started = APP_CONTROL_RESULT_APP_STARTED,
+    Succeeded = APP_CONTROL_RESULT_SUCCEEDED,
+    Failed = APP_CONTROL_RESULT_FAILED,
+    Cancelled = APP_CONTROL_RESULT_CANCELED
+  };
+
+  AppControl(app_control_h app_control);
+  ~AppControl();
+
+  int GetId() { return id_; }
+  app_control_h Handle() { return handle_; }
+
+  AppControlResult GetOperation(std::string& operation);
+  AppControlResult SetOperation(const std::string& operation);
+  AppControlResult GetUri(std::string& uri);
+  AppControlResult SetUri(const std::string& uri);
+  AppControlResult GetMime(std::string& mime);
+  AppControlResult SetMime(const std::string& mime);
+  AppControlResult GetCategory(std::string& category);
+  AppControlResult SetCategory(const std::string& category);
+  AppControlResult GetAppId(std::string& app_id);
+  AppControlResult SetAppId(const std::string& app_id);
+  AppControlResult GetComponentId(std::string& component_id);
+  AppControlResult SetComponentId(const std::string& component_id);
+  AppControlResult GetCaller(std::string& caller);
+  AppControlResult GetLaunchMode(LaunchMode& launch_mode);
+  AppControlResult SetLaunchMode(const LaunchMode launch_mode);
+
+  AppControlResult Reply(AppControl* reply, Result result);
+
+ private:
+  AppControlResult GetString(std::string& str, int func(app_control_h, char**));
+  AppControlResult SetString(const std::string& str,
+                             int func(app_control_h, const char*));
+  AppControlResult ReadExtraData();
+
+  AppControlExtraData extra_data_;
+  app_control_h handle_;
   int id_;
   static int next_id_;
 };
@@ -79,7 +155,9 @@ class AppControlChannel {
   // that EventChannel on Dart side will be registered
   // before native OnAppControl event
   // TODO: Add limit for queue elements
-  std::queue<std::unique_ptr<AppControl>> queue_;
+  std::queue<int> queue_;
+
+  std::unordered_map<int, std::unique_ptr<AppControl>> map_;
 };
 
 }  // namespace flutter
